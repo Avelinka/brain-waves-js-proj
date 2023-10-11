e74c22d553e51f72f307c9829a13d57e7be51f03
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-10-11 20:18:08 +0300

    123

--- src/fonds-ukraine.js
+++ src/fonds-ukraine.js
@@ -1,13 +1,54 @@
-import Splide from 'splide';
+import Splide from '@splidejs/splide';
 
 const funds = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: 'save_the_children.png',
-  },
-  // Add more fund objects here...
-];
+    {
+      title: 'Save the Children',
+      url:
+        'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+      img: null,
+    },
+    {
+      title: 'Project HOPE',
+      url: 'https://www.projecthope.org/country/ukraine/',
+      img: null,
+    },
+    {
+      title: 'UNITED24',
+      url: 'https://u24.gov.ua/uk',
+      img: null,
+    },
+    {
+      title: 'International Medical Corps',
+      url: 'https://internationalmedicalcorps.org/country/ukraine/',
+      img: null,
+    },
+    {
+      title: 'Medicins Sans Frontieres',
+      url: 'https://www.msf.org/ukraine',
+      img: null,
+    },
+    {
+      title: 'RAZOM',
+      url: 'https://www.razomforukraine.org/',
+      img: null,
+    },
+    {
+      title: 'Action against hunger',
+      url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+      img: null,
+    },
+    {
+      title: 'World vision',
+      url: 'https://www.wvi.org/emergencies/ukraine',
+      img: null,
+    },
+    {
+      title: 'Serhiy Prytula Charity Foundation',
+      url: 'https://prytulafoundation.org/en',
+      img: null,
+    },
+  ];
+  
 
 const supportFonds = document.querySelector('.support-fonds ul');
 const slideBtn = document.querySelector('.slide-btn');
