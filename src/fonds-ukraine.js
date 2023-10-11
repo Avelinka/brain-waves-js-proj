d64d35157f455308b250bf6fcab2f937c8884f60
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-10-11 19:33:36 +0300

    123

--- src/fonds-ukraine.js
+++ src/fonds-ukraine.js
@@ -1,1 +1,46 @@
-import Splide from '@splidejs/splide';
+import Splide from 'splide';
+
+const funds = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: 'save_the_children.png',
+  },
+  // Add more fund objects here...
+];
+
+const supportFonds = document.querySelector('.support-fonds ul');
+const slideBtn = document.querySelector('.slide-btn');
+
+function createFundItem(fund) {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+    <a href="${fund.url}" target="_blank">
+      <img src="${fund.img}" alt="${fund.title}" />
+    </a>
+  `;
+  return listItem;
+}
+
+function initializeSplide() {
+  new Splide('.splide', {
+    perPage: 1,
+    perMove: 1,
+    rewind: true,
+  }).mount();
+}
+
+function addFundsToSlider() {
+  funds.forEach((fund) => {
+    supportFonds.appendChild(createFundItem(fund));
+  });
+  initializeSplide();
+}
+
+slideBtn.addEventListener('click', () => {
+  // Scroll to the next slide on button click
+  const splide = Splide.get('.splide');
+  splide.go('+1');
+});
+
+addFundsToSlider();
