7c2ac85cc6f17d8f0df86aadd19f6482c5267a53
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-10-12 18:16:10 +0300

    123

--- src/js/fonds-ukraine.js
+++ src/js/fonds-ukraine.js
@@ -1,87 +1,83 @@
-import Splide from '@splidejs/splide';
+
 
 const funds = [
-    {
-      title: 'Save the Children',
-      url:
-        'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-      img: null,
-    },
-    {
-      title: 'Project HOPE',
-      url: 'https://www.projecthope.org/country/ukraine/',
-      img: null,
-    },
-    {
-      title: 'UNITED24',
-      url: 'https://u24.gov.ua/uk',
-      img: null,
-    },
-    {
-      title: 'International Medical Corps',
-      url: 'https://internationalmedicalcorps.org/country/ukraine/',
-      img: null,
-    },
-    {
-      title: 'Medicins Sans Frontieres',
-      url: 'https://www.msf.org/ukraine',
-      img: null,
-    },
-    {
-      title: 'RAZOM',
-      url: 'https://www.razomforukraine.org/',
-      img: null,
-    },
-    {
-      title: 'Action against hunger',
-      url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-      img: null,
-    },
-    {
-      title: 'World vision',
-      url: 'https://www.wvi.org/emergencies/ukraine',
-      img: null,
-    },
-    {
-      title: 'Serhiy Prytula Charity Foundation',
-      url: 'https://prytulafoundation.org/en',
-      img: null,
-    },
-  ];
-  
+  {
+    title: 'Save the Children',
+    url:
+      'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: null,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: null,
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: null,
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: null,
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: null,
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: null,
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: null,
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: null,
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: null,
+  },
+];
 
-const supportFonds = document.querySelector('.support-fonds ul');
-const slideBtn = document.querySelector('.slide-btn');
+const slideList = document.querySelector('.splide__list');
+const slides = [];
 
-function createFundItem(fund) {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `
-    <a href="${fund.url}" target="_blank">
-      <img src="${fund.img}" alt="${fund.title}" />
-    </a>
-  `;
-  return listItem;
-}
-
-function initializeSplide() {
-  new Splide('.splide', {
-    perPage: 1,
-    perMove: 1,
-    rewind: true,
-  }).mount();
-}
-
-function addFundsToSlider() {
-  funds.forEach((fund) => {
-    supportFonds.appendChild(createFundItem(fund));
-  });
-  initializeSplide();
-}
-
-slideBtn.addEventListener('click', () => {
-  // Scroll to the next slide on button click
-  const splide = Splide.get('.splide');
-  splide.go('+1');
+funds.forEach((fund) => {
+  const slide = document.createElement('li');
+  slide.className = 'splide__slide';
+  const fundLink = document.createElement('a');
+  fundLink.href = fund.url;
+  fundLink.textContent = fund.title;
+  slide.appendChild(fundLink);
+  slides.push(slide);
 });
 
-addFundsToSlider();
+
+slides.forEach((slide) => {
+  slideList.appendChild(slide);
+});
+
+const Slider = new Splide('#sliderOne', {
+  perPage: 6,
+  gap: '20px',
+  pagination: false,
+  direction: 'ttb',
+  height: '292px',
+  dynamicSlides: true,
+  rewind: true,
+});
+
+
+
+
+Slider.mount();
