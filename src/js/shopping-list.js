62c5a5bb209e2f772d34975ab8164bb354e2fe7d
Avelinka <131503535+Avelinka@users.noreply.github.com>
2023-10-15 20:31:31 +0300

    change2

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,4 +1,4 @@
-import { getInformationById } from './fetch-requests';
+// import { getInformationById } from './fetch-requests';
 
 const goToCartButton = document.querySelector('.js-go-to-cart');
 const refJsLIst = document.querySelector('.js-list');
