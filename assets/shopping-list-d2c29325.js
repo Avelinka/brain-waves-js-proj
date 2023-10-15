6fa8f370ee2bf166eea9079e49cf0aec51b17871
Avelinka <131503535+Avelinka@users.noreply.github.com>
2023-10-15 13:47:55 +0000

    Deploying to gh-pages from @ Avelinka/brain-waves-js-proj@a6ef6f8b76d5086a7d76d340411bd5328850079b 🚀

--- assets/shopping-list-d2c29325.js
+++ assets/shopping-list-d2c29325.js
@@ -1,4 +1,4 @@
-import"./notiflix-aio-3.2.6.min-ff94ac69.js";document.querySelector(".js-go-to-cart");document.querySelector(".js-list");const d=document.querySelector(".js-list"),n="shoppingList",c=JSON.parse(localStorage.getItem(n))||[];function r(e){return e.map(({author:t,book_image:o,buy_links:i,description:a,title:s})=>`
+import"./notiflix-aio-3.2.6.min-d7bf98f2.js";document.querySelector(".js-go-to-cart");document.querySelector(".js-list");const d=document.querySelector(".js-list"),n="shoppingList",c=JSON.parse(localStorage.getItem(n))||[];function r(e){return e.map(({author:t,book_image:o,buy_links:i,description:a,title:s})=>`
     <li data-id="${s}" class="js-product shopping-list-item">
         <img src="${o}" alt="${s}" width="116" height="165" class="shop-img"/> 
         <div class="shop-item-div">
