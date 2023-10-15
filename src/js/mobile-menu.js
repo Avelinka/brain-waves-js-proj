08ef9b51958f6b99c364de2237f0089673e504f5
Avelinka <131503535+Avelinka@users.noreply.github.com>
2023-10-15 20:49:50 +0300

    small correct

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,62 +1,66 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn1 = document.querySelector('.js-close-menu-1');
-    const closeMenuBtn2 = document.querySelector('.js-close-menu-2');
-    const burgerBtn = document.querySelector('#js-icon');
-//const closeMenuBtnIcon = document.querySelector('.js-close-menu-icon'); //  кнопка з хрестиком
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn1 = document.querySelector('.js-close-menu-1');
+  const closeMenuBtn2 = document.querySelector('.js-close-menu-2');
+  const burgerBtn = document.querySelector('#js-icon');
+  //const closeMenuBtnIcon = document.querySelector('.js-close-menu-icon'); //  кнопка з хрестиком
 
-    let isMenuOpen = false;
-    
-    const toggleMenu = () => {
-        isMenuOpen = !isMenuOpen;
+  let isMenuOpen = false;
 
-        if (isMenuOpen) {
-            burgerBtn.setAttribute('href', new URL('../images/sprite.svg#icon-x-close', window.location).href);
-            document.body.classList.toggle('no-scroll');
-        } else {
-            burgerBtn.setAttribute('href', new URL('../images/sprite.svg#icon-gamburger', window.location).href);
-            document.body.classList.toggle('no-scroll');
-        }
-    
-        openMenuBtn.setAttribute('aria-expanded', isMenuOpen);
-        mobileMenu.classList.toggle('is-open', isMenuOpen);
+  const toggleMenu = () => {
+    isMenuOpen = !isMenuOpen;
 
-        if (isMenuOpen) {
-            document.addEventListener('keydown', handleEscKeyPress);
-        } else {
-            document.removeEventListener('keydown', handleEscKeyPress)
-        }
-    };
+    if (isMenuOpen) {
+      burgerBtn.setAttribute(
+        'href',
+        new URL('/images/sprite.svg#icon-x-close', window.location).href
+      );
+      document.body.classList.toggle('no-scroll');
+    } else {
+      burgerBtn.setAttribute(
+        'href',
+        new URL('/images/sprite.svg#icon-gamburger', window.location).href
+      );
+      document.body.classList.toggle('no-scroll');
+    }
 
+    openMenuBtn.setAttribute('aria-expanded', isMenuOpen);
+    mobileMenu.classList.toggle('is-open', isMenuOpen);
 
+    if (isMenuOpen) {
+      document.addEventListener('keydown', handleEscKeyPress);
+    } else {
+      document.removeEventListener('keydown', handleEscKeyPress);
+    }
+  };
 
-    const closeModal = () => {
-        isMenuOpen = false;
-        burgerBtn.setAttribute('href', '/images/sprite.svg#icon-gamburger');
-        mobileMenu.classList.remove('is-open');
-        openMenuBtn.setAttribute('aria-expanded', false);
-        document.removeEventListener('click', closeModal);
-        document.removeEventListener('keydown', handleEscKeyPress);
-    };
+  const closeModal = () => {
+    isMenuOpen = false;
+    burgerBtn.setAttribute('href', '/images/sprite.svg#icon-gamburger');
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    document.removeEventListener('click', closeModal);
+    document.removeEventListener('keydown', handleEscKeyPress);
+  };
 
-    const handleEscKeyPress = event => {
-        if (event.key === 'Escape') {
-            closeModal();
-        }
-    };
+  const handleEscKeyPress = event => {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  };
 
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn1.addEventListener('click', toggleMenu);
-    closeMenuBtn2.addEventListener('click', toggleMenu);
-    //closeMenuBtnIcon.addEventListener('click', closeModal); //  слухач подій для хрестика
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn1.addEventListener('click', toggleMenu);
+  closeMenuBtn2.addEventListener('click', toggleMenu);
+  //closeMenuBtnIcon.addEventListener('click', closeModal); //  слухач подій для хрестика
 
-    document.addEventListener('keydown', handleEscKeyPress);
+  document.addEventListener('keydown', handleEscKeyPress);
 
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-        if (!e.matches) return;
-        mobileMenu.classList.remove('is-open');
-        openMenuBtn.setAttribute('aria-expanded', false);
-        bodyScrollLock.enableBodyScroll(document.body);
-    });
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
